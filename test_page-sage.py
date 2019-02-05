e7b1c2e6841e138a5b01154ef6ff25e16fe249b6
Peter Nielson <computergeekpete@gmail.com>
2019-02-04 22:54:06 -0700

    Added unit test file for routes

---
+++ test_page-sage.py
@@ -0,0 +1,141 @@
+import os
+import tempfile
+
+#import pytest
+import unittest
+
+from app import app
+
+'''
+@pytest.fixture
+def client():
+    app.app.config["TESTING"] = True
+    client = app.app.test_client()
+
+    yield client
+'''
+
+class BasicRouteTests(unittest.TestCase):
+
+    def setUp(self):
+        app.config["TESTING"] = True
+        app.config["WTF_CSRF_ENABLED"] = False
+        app.config["DEBUG"] = False
+        
+        self.app = app.test_client()
+
+        self.assertEqual(app.debug, False)
+
+    def tearDown(self):
+        pass
+
+    #########################
+    ## Landing Page Routes ##
+    #########################
+
+    def test_landing_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_page(self):
+        response = self.app.get('/index', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_welcome_page(self):
+        response = self.app.get('/welcome', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page(self):
+        response = self.app.get('/about', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+  
+    ##################
+    ## AuthN Routes ##
+    ##################
+
+    def test_login_page(self):
+        response = self.app.get('/login')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup(self):
+        response = self.app.get('/signup')
+        self.assertEqual(response.status_code, 200)
+
+
+    #################
+    ## User Routes ##
+    #################
+
+    def test_user_page(self):
+        response = self.app.get('/user')
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile_page(self):
+        response = self.app.get('/profile')
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_book(self):
+        response = self.app.get('/user/book')
+        self.assertEqual(response.status_code, 200)
+
+    def test_my_shelf(self):
+        response = self.app.get('/my-shelf')
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_search(self):
+        response = self.app.get('/user/search')
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_settings(self):
+        response = self.app.get('user/settings')
+        self.assertEqual(response.status_code, 200)
+
+
+    #####################
+    ## Bookclub Routes ##
+    #####################
+
+    def test_bookclub(self):
+        response = self.app.get('/bookclub')
+        self.assertEqual(response.status_code, 200)
+
+    def test_club(self):
+        response = self.app.get('/club')
+        self.assertEqual(response.status_code, 200)
+
+    def test_forums(self):
+        response = self.app.get('/bookclub/forums')
+        self.assertEqual(response.status_code, 200)
+
+    def test_forum(self):
+        response = self.app.get('/bookclub/forum')
+        self.assertEqual(response.status_code, 200)
+
+    def test_bookclub_shelf(self):
+        response = self.app.get('/bookclub/shelf')
+        self.assertEqual(response.status_code, 200)
+
+    def test_bookclub_bookshelf(self):
+        response = self.app.get('/bookclub/bookshelf')
+        self.assertEqual(response.status_code, 200)
+
+    def test_bookclub_suggestions(self):
+        response = self.app.get('/bookclub/suggestions')
+        self.assertEqual(response.status_code, 200)
+
+    def test_bookclub_book(self):
+        response = self.app.get('/bookclub/book')
+        self.assertEqual(response.status_code, 200)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+'''
+def test_welcome_page():
+    with app.test_client() as cl:
+        response = cl.get('/index')
+        assert response.status_code, 404
+'''
