ccb50390c5de4ea69ca87ad635dd7eb38a808c2c
lupini <elupini@msudenver.edu>
2019-02-17 17:46:40 -0700

    Finished header/sidebar styling, working on main pages

--- app/static/js/user.js
+++ app/static/js/user.js
@@ -1,10 +1,14 @@
 
 const sidebar = $('#sidebar-bg').get(0);
+const main = $('#main').get(0);
 
 function toggleSidebar() {
     if (sidebar.style.display === "none") {
+        main.style.marginLeft="10%";
+        sidebar.style.width="10%";
         sidebar.style.display = "block";
     } else {
+        main.style.marginLeft="0%";
         sidebar.style.display = "none";
     }
 }
